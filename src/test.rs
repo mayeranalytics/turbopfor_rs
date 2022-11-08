e420b31bf14c2cdb3bd77ae0ef385943a7a48400
Markus Mayer <mmayer@mayeranalytics.com>
2022-11-08 08:23:02 +0100

    streamline tests

--- src/test.rs
+++ src/test.rs
@@ -1,10 +1,12 @@
 use super::codec::*;
-use generate_random::GenerateRandom;
-use std::fmt::Debug;
-use rand::rngs::ThreadRng;
-use rand::Rng;
-use num::{Zero};
-
+use std::{
+    fmt::Debug,
+    collections::HashSet, hash::Hash,
+};
+use rand::{
+    rngs::ThreadRng,
+    Rng, 
+};
 
 const MAX_TEST_LEN: usize = 1024 * 1024;
 const N_ITERATIONS: usize = 128;
@@ -42,12 +44,82 @@ fn simple_test()
     assert!(compare(&v, &output[..v.len()]));
 }
 
+#[derive(PartialEq, Eq)]
+enum DataType {
+    Increasing,
+    StrictlyIncreasing,
+    Unsorted
+}
+
+trait Num where Self: Sized {
+    fn zero() -> Self;
+    fn upper_bound() -> usize;
+    fn rand(rng: &mut ThreadRng) -> Self;
+}
+
+impl Num for u8 {
+    fn zero() -> u8 { 0 }
+    fn upper_bound() -> usize { 255 }
+    fn rand(rng: &mut ThreadRng) -> u8 { rng.gen() }
+}
+
+impl Num for u16 {
+    fn zero() -> u16 { 0 }
+    fn upper_bound() -> usize { 65535 }
+    fn rand(rng: &mut ThreadRng) -> u16 { rng.gen() }
+}
+
+impl Num for u32 {
+    fn zero() -> u32 { 0 }
+    fn upper_bound() -> usize { 4294967295 }
+    fn rand(rng: &mut ThreadRng) -> u32 { rng.gen() }
+}
+
+impl Num for u64 {
+    fn zero() -> u64 { 0 }
+    fn upper_bound() -> usize { 18446744073709551615 } // you better be on a 64 bit system
+    fn rand(rng: &mut ThreadRng) -> u64 { rng.gen() }
+}
+
+/// Make increasing data of maximum length `max_len`. If `strictly` is true, make strictly increasing data.
+fn mk_data_inc<T: Num+Clone+std::ops::AddAssign+Hash+Eq+Ord>(
+    max_len: usize,
+    rng: &mut ThreadRng,
+    data_type: &DataType
+) -> Vec<T>
+{
+    match data_type {
+        DataType::StrictlyIncreasing => {
+            let len = rng.gen_range(1..std::cmp::min(max_len, T::upper_bound()));
+            let mut data: HashSet<T> = HashSet::new();
+            for _ in 0..len { data.insert(T::rand(rng)); }
+            let mut data: Vec<T> = data.into_iter().collect();
+            data.sort();
+            data
+        },
+        DataType::Increasing => {
+            let len = rng.gen_range(1..max_len);
+            let mut data: Vec<T> = Vec::with_capacity(MAX_TEST_LEN);
+            for _ in 0..len { data.push(T::rand(rng)); }
+            data.sort();
+            data
+        },
+        DataType::Unsorted => {
+            let len = rng.gen_range(1..max_len);
+            let mut data: Vec<T> = Vec::with_capacity(MAX_TEST_LEN);
+            for _ in 0..len { data.push(T::rand(rng)); }
+            data
+        }
+    }
+}
+
+
 /// Checks if data was written beyond `len`. returns true if ok
-fn check_no_overflow(data: &[u8], len: usize) -> bool {
+fn check_no_overflow<T: Ord+Debug+Num>(data: &[T], len: usize) -> bool {
     let mut i = data.len();
     while i > 0 {
         i -= 1;
-        if data[i] != 0 { break; }
+        if data[i] != T::zero() { break; }
     }
     if i > len {
         println!("empty data starts at {}, {} after len (={})", i, i-len+1, len);
@@ -59,179 +131,128 @@ fn check_no_overflow(data: &[u8], len: usize) -> bool {
     }
 }
 
-fn test_enc<W:Width<W>, T: GenerateRandom + Eq + Codec<W> + Debug + Zero + Copy>() 
+fn test_generic<W:Width<W>, T: Num+Clone+std::ops::AddAssign+Hash+Eq+Ord+Debug>(
+    max_test_len: usize,
+    enc: fn(&[T], &mut [u8]) -> usize,
+    dec: fn(&[u8], usize, &mut [T]) -> usize,
+    data_type: DataType
+) 
 {
     let mut rng = rand::thread_rng();
 
     // alloc
-    let enc_size: usize = W::buf_size::<T>(MAX_TEST_LEN);
-    let dec_size: usize = MAX_TEST_LEN+32;
+    let enc_size: usize = W::buf_size::<T>(max_test_len);
+    let dec_size: usize = max_test_len+32;
     let mut encoded: Vec<u8> = vec![0; enc_size];
     let mut decoded: Vec<T> = vec![T::zero(); dec_size];
 
     // repeat test
     for _ in 0..N_ITERATIONS {
         // make data
-        let len = rng.gen_range(1..MAX_TEST_LEN);   // length of randomly generated data
-        let mut input: Vec<T> = Vec::with_capacity(MAX_TEST_LEN);
-        for _ in 0..len                  { input.push(T::generate_random(&mut rng)); }
-        while input.len() < MAX_TEST_LEN { input.push(T::zero()); }
+        let len = rng.gen_range(1..max_test_len);   // length of randomly generated data
+        let mut input: Vec<T> = mk_data_inc(len, &mut rng, &data_type);
+        while input.len() < max_test_len { input.push(T::zero()); }
         // encode
-        let n_bytes_written = T::enc(
+        let n_bytes_written = enc(
             &input, 
             &mut encoded
         );
         assert!(check_no_overflow(&encoded, n_bytes_written));
         // decode
-        let n_bytes_read = T::dec(
+        let n_bytes_read = dec(
             &mut encoded,
             input.len(),
             &mut decoded
         );
         assert_eq!(n_bytes_written, n_bytes_read);
         assert!(compare(&input, &decoded[..input.len()]));
+        assert!(check_no_overflow(&decoded, len));
 
-        // Clean up
+        // Reset buffers
         for i in 0..encoded.len() { encoded[i] = 0; }
+        for i in 0..decoded.len() { decoded[i] = T::zero(); }
     }
 }
 
+
+//////////////////////////////////////// enc ////////////////////////////////////////
+
 #[test]
 fn test_enc8() {
-    test_enc::<W,u8>();
+    test_generic::<W, u8>(MAX_TEST_LEN, Codec::<W>::enc,     Codec::<W>::dec    , DataType::Unsorted);
 }
 
 #[test]
 fn test_enc16() {
-    test_enc::<W,u16>();
-    test_enc::<W128v,u16>();
+    test_generic::<W,     u16>(MAX_TEST_LEN, Codec::<W>::enc, Codec::<W>::dec, DataType::Unsorted);
+    test_generic::<W128v, u16>(MAX_TEST_LEN, Codec::<W>::enc, Codec::<W>::dec, DataType::Unsorted);
 }
 
 #[test]
 fn test_enc32() {
-    test_enc::<W,u32>();
-    test_enc::<W128v,u32>();
+    test_generic::<W,     u32>(MAX_TEST_LEN, Codec::<W>::enc, Codec::<W>::dec, DataType::Unsorted);
+    test_generic::<W128v, u32>(MAX_TEST_LEN, Codec::<W>::enc, Codec::<W>::dec, DataType::Unsorted);
 }
 
 #[test]
 fn test_enc64() {
-    test_enc::<W,u64>();
+    test_generic::<W, u64>(MAX_TEST_LEN, Codec::<W>::enc, Codec::<W>::dec, DataType::Unsorted);
 }
 
-trait Num where Self: Sized {
-    fn mk_data(len: usize, rng: &mut ThreadRng, positive:bool) -> Vec<Self>;
-}
-
-impl Num for u16 {
-    fn mk_data(len: usize, rng: &mut ThreadRng, positive:bool) -> Vec<u16> {
-        let mut input: Vec<u16> = vec![0; len+32];
-        let mut x = 0;
-        for _ in 0..len {
-            x += rng.gen_range((if positive {1} else {0})..32);
-            input.push(x);
-        }        
-        input
-    }
-}
-
-impl Num for u32 {
-    fn mk_data(len: usize, rng: &mut ThreadRng, positive:bool) -> Vec<u32> {
-        let mut input: Vec<u32> = vec![0; len+32];
-        let mut x = 0;
-        for _ in 0..len {
-            x += rng.gen_range((if positive {1} else {0})..256);
-            input.push(x);
-        }        
-        input
-    }
-}
-
-impl Num for u64 {
-    fn mk_data(len: usize, rng: &mut ThreadRng, positive:bool) -> Vec<u64> {
-        let mut input: Vec<u64> = vec![0; len+32];
-        let mut x = 0;
-        for _ in 0..len {
-            x += rng.gen_range((if positive {1} else {0})..256*256);
-            input.push(x);
-        }        
-        input
-    }
-}
-
-fn test_generic<W:Width<W>, T: GenerateRandom + Eq + Codec<W> + Zero + Clone + Num>(
-    max_test_len: usize,
-    enc: fn(&[T], &mut [u8]) -> usize,
-    dec: fn(&[u8], usize, &mut [T]) -> usize
-) 
-{
-    let mut rng = rand::thread_rng();
-    let mut encoded: Vec<u8> = vec![0; W::buf_size::<T>(max_test_len)];
-    let mut decoded: Vec<T> = vec![T::zero(); W::buf_size::<T>(max_test_len+32)];
-    for _ in 0..32 {
-        let len = rng.gen_range(1..max_test_len);
-        let input: Vec<T> = T::mk_data(len, &mut rng, false);
-        let enc_size = enc(
-            &input,
-            &mut encoded
-        );
-        let bytes_used = dec(
-            &mut encoded,
-            input.len(),
-            &mut decoded
-        );
-        assert_eq!(enc_size, bytes_used);
-        assert!(compare(&input, &decoded[..input.len()]));
-    }
-}
+//////////////////////////////////////// denc ////////////////////////////////////////
 
 #[test]
 fn test_denc16() {
-    test_generic::<W, u16>(2047, Codec::<W>::denc, Codec::<W>::ddec);
-    test_generic::<W128v, u16>(2047, Codec::<W128v>::denc, Codec::<W128v>::ddec);
+    test_generic::<W,     u16>(MAX_TEST_LEN, Codec::<W>::denc,     Codec::<W>::ddec    , DataType::Increasing);
+    test_generic::<W128v, u16>(MAX_TEST_LEN, Codec::<W128v>::denc, Codec::<W128v>::ddec, DataType::Increasing);
 }
 
 #[test]
 fn test_denc32() {
-    test_generic::<W, u32>(MAX_TEST_LEN, Codec::<W>::denc, Codec::<W>::ddec);
-    test_generic::<W128v, u32>(2047, Codec::<W128v>::denc, Codec::<W128v>::ddec);
-    test_generic::<W256v, u32>(2047, Codec::<W256v>::denc, Codec::<W256v>::ddec);
+    test_generic::<W,     u32>(MAX_TEST_LEN, Codec::<W>::denc,     Codec::<W>::ddec    , DataType::Increasing);
+    test_generic::<W128v, u32>(MAX_TEST_LEN, Codec::<W128v>::denc, Codec::<W128v>::ddec, DataType::Increasing);
+    test_generic::<W256v, u32>(MAX_TEST_LEN, Codec::<W256v>::denc, Codec::<W256v>::ddec, DataType::Increasing);
 }
 
 #[test]
 fn test_denc64() {
-    test_generic::<W, u64>(MAX_TEST_LEN, Codec::<W>::denc, Codec::<W>::ddec);
+    test_generic::<W, u64>(MAX_TEST_LEN, Codec::<W>::denc, Codec::<W>::ddec, DataType::Increasing);
 }
+
+//////////////////////////////////////// d1enc ////////////////////////////////////////
 
 #[test]
 fn test_d1enc16() {
-    test_generic::<W, u16>(2047, Codec::<W>::d1enc, Codec::<W>::d1dec);
-    test_generic::<W128v, u16>(2047, Codec::<W128v>::d1enc, Codec::<W128v>::d1dec);
+    test_generic::<W,     u16>(MAX_TEST_LEN, Codec::<W>::d1enc,     Codec::<W>::d1dec,     DataType::StrictlyIncreasing);
+    test_generic::<W128v, u16>(MAX_TEST_LEN, Codec::<W128v>::d1enc, Codec::<W128v>::d1dec, DataType::StrictlyIncreasing);
 }
 
 #[test]
 fn test_d1enc32() {
-    test_generic::<W, u32>(MAX_TEST_LEN, Codec::<W>::d1enc, Codec::<W>::d1dec);
+    test_generic::<W, u32>(MAX_TEST_LEN, Codec::<W>::d1enc, Codec::<W>::d1dec, DataType::StrictlyIncreasing);
 }
 
 #[test]
 fn test_d1enc64() {
-    test_generic::<W, u64>(MAX_TEST_LEN, Codec::<W>::d1enc, Codec::<W>::d1dec);
+    test_generic::<W, u64>(MAX_TEST_LEN, Codec::<W>::d1enc, Codec::<W>::d1dec, DataType::StrictlyIncreasing);
 }
+
+//////////////////////////////////////// zenc ////////////////////////////////////////
 
 #[test]
 fn test_zenc16() {
-    test_generic::<W, u16>(2047, Codec::<W>::zenc, Codec::<W>::zdec);
-    test_generic::<W128v, u16>(2047, Codec::<W128v>::zenc, Codec::<W128v>::zdec);
+    test_generic::<W,     u16>(MAX_TEST_LEN, Codec::<W>::zenc,     Codec::<W>::zdec    , DataType::Unsorted);
+    test_generic::<W128v, u16>(MAX_TEST_LEN, Codec::<W128v>::zenc, Codec::<W128v>::zdec, DataType::Unsorted);
 }
 
 #[test]
 fn test_zenc32() {
-    test_generic::<W, u32>(MAX_TEST_LEN, Codec::<W>::zenc, Codec::<W>::zdec);
-    test_generic::<W128v, u32>(2047, Codec::<W128v>::zenc, Codec::<W128v>::zdec);
-    test_generic::<W256v, u32>(2047, Codec::<W256v>::zenc, Codec::<W256v>::zdec);
+    test_generic::<W,     u32>(MAX_TEST_LEN, Codec::<W>::zenc,     Codec::<W>::zdec    , DataType::Unsorted);
+    test_generic::<W128v, u32>(MAX_TEST_LEN, Codec::<W128v>::zenc, Codec::<W128v>::zdec, DataType::Unsorted);
+    test_generic::<W256v, u32>(MAX_TEST_LEN, Codec::<W256v>::zenc, Codec::<W256v>::zdec, DataType::Unsorted);
 }
 
 #[test]
 fn test_zenc64() {
-    test_generic::<W, u64>(MAX_TEST_LEN, Codec::<W>::zenc, Codec::<W>::zdec);
+    test_generic::<W, u64>(MAX_TEST_LEN, Codec::<W>::zenc, Codec::<W>::zdec, DataType::Unsorted);
 }
